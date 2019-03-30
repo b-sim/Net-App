9aa39bb4b1c697817833d82bb3f0e71376034639
b-sim <simonb@live.ca>
2019-03-30 15:30:05 -0400

    setté url serveur heroku

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import store from '@/store/store';
 
 export default () => {
     return axios.create({
-        baseURL: `http://ser.netblb.com/`,
+        baseURL: `https://formulaire-ser.herokuapp.com/`,
         // baseURL: `http://localhost:8081/`,
         headers: {
             Authorization: `Bearer ${store.state.token}`
